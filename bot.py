17ba997d3ecd202aa217c3d62918dea45d6ba5c3
Natsumikan2782 <86223797+Natsumikan2782@users.noreply.github.com>
2022-04-04 00:07:55 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,17 +22,28 @@ async def on_message(message: Message):
         return
     
     elif message.content.startswith("!trans"):
-        args = message.content.split()
-        req = f"https://script.google.com/macros/s/AKfycbzZtvOvf14TaMdRIYzocRcf3mktzGgXvlFvyczo/exec?text={' '.join(args[3:])}&source={args[1]}&target={args[2]}"
-        date = requests.get(req).json()
-        word = date["text"]
+        embed1 = discord.Embed(title="翻訳中...", description="",color=0xa88dfa)
+        embed1.set_footer(text = "Powered by natsumi#7504")
+        msg = await message.channel.send(embed = embed1)
         
-        embed = discord.Embed(title=f"{args[1]}➪{args[2]}", description="",color=0xa88dfa)
-        embed.add_field(name="原文",value="``" + ' '.join(args[3:]) + "``",inline=False)
-        embed.add_field(name="翻訳",value="``"+word+"``")
-        embed.set_footer(text = "Powered by natsumi#7504")
+        try:
+            args = message.content.split()
+            key = config["api-key"]
+            req = f"https://api-free.deepl.com/v2/translate?auth_key={key}&text={' '.join(args[3:])}&source_lang={args[1]}&target_lang={args[2]}"
+            date = requests.get(req).json()
+            word = date["translations"][0]["text"]
         
-        await message.channel.send(embed=embed)
+            embed2 = discord.Embed(title=f"{args[1]}➪{args[2]}", description="",color=0xa88dfa)
+            embed2.add_field(name="原文",value="``" + ' '.join(args[3:]) + "``",inline=False)
+            embed2.add_field(name="翻訳",value="``"+word+"``")
+            embed2.set_footer(text = "Powered by natsumi#7504")
+        
+            await msg.edit(embed=embed2)
+            
+        except KeyError:
+            embed = discord.Embed(title="翻訳に失敗しました、言語が不正です。", description="",color=0xa88dfa)
+            embed.set_footer(text = "Powered by natsumi#7504")
+            await msg.edit(embed=embed)
     
 TOKEN = config["token"]
 client.run(TOKEN)    
